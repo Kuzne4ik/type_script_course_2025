dbc8d373b27de6bd0831a67ab8140fff24df9786
Kuzne4ik <sidorov.a@tm.biz-apps.ru>
2026-03-07 10:52:15 +0300

    feat(types): add debug type definitions

---
+++ src/types/debug.types.ts
@@ -0,0 +1,14 @@
+/*
+import type { ComponentProps } from "react"
+import type { IPlayerState } from "./player.type"
+
+// Если не установлено расширение для чтения полей в всплывающие подсказки то можно использовать перечитку в тип
+type Debug<T> = { [K in keyof T]: T[K]}
+// Пример: перчитка в тип 
+type CheckType = Debug<IPlayerState>;
+
+
+// Если не установлено расширение для чтения полей в всплывающие подсказки то можно использовать перечитку в тип
+type ButtonProps = ComponentProps<'button'>;
+*/
+
